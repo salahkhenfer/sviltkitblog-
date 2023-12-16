07d0d194de259c08177e3c229de8c666e4a7a17c
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2023-12-16 18:59:13 +0100

    add style page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-import config from '/src/config'
-import SEO from '$lib/SEO.svelte'
-import PostSection from '$lib/PostSection.svelte'
-import ArchiveSection from '$lib/ArchiveSection.svelte'
+// import config from '/src/config'
+// import SEO from '$lib/SEO.svelte'
+// import PostSection from '$lib/PostSection.svelte'
+// import ArchiveSection from '$lib/ArchiveSection.svelte'
 import Main from '$lib/main.svelte'
 
-export let data
+// export let data
 </script>
 
 <Main/>
